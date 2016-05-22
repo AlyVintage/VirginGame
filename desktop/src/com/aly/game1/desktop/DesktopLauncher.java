822ede0d8929d558777f0bc1a36b3d7c085de13e
Przemysław Niedzielski <aly.poczta@windowslive.com>
2016-05-22 11:49:50 +0200

    init

---
+++ desktop/src/com/aly/game1/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.aly.game1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aly.game1.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+		//dsd
+	}
+}
