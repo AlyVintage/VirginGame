b9053881204dacd8734c44ba428d04ef3c79f5df
Przemysław Niedzielski <aly.poczta@windowslive.com>
2016-05-22 13:26:11 +0200

    init config

--- core/src/com/aly/game1/MyGdxGame.java
+++ core/src/com/aly/game1/MyGdxGame.java
@@ -1,27 +1,30 @@
 package com.aly.game1;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import Screens.SplashScreen;
+
+public class MyGdxGame extends Game {
+//	SpriteBatch batch;
+//	Texture img;
+	public final static String GameName = "Virgin";
+	public static int WIDTH = 800;
+	public static int HEIGHT = 600;
+
+	private boolean paused;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
+		this.setScreen(new SplashScreen(this));
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		//batch.draw(img, 0, 0);
-		batch.end();
+
+//
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
 	}
 }
--- desktop/src/com/aly/game1/desktop/DesktopLauncher.java
+++ desktop/src/com/aly/game1/desktop/DesktopLauncher.java
@@ -1,13 +1,18 @@
 package com.aly.game1.desktop;
 
+import com.aly.game1.MyGdxGame;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.aly.game1.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = MyGdxGame.GameName;
+		config.width = MyGdxGame.WIDTH;
+		config.height = MyGdxGame.HEIGHT;
+		config.resizable = false;
 		new LwjglApplication(new MyGdxGame(), config);
-		//dsd
+
 	}
 }
